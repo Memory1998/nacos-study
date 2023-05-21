820d39c5cf595e7253e5b8edd8964f650f72e6ba
gwx <breeze-cloud@foxmail.com>
2023-05-21 17:56:18 +0800

    demo nacos-sdk

--- nacos-sdk-study/src/com/study/nacos/Main.java
+++ nacos-sdk-study/src/com/study/nacos/Main.java
@@ -1,7 +1,20 @@
 package com.study.nacos;
 
-public class Main {
-    public static void main(String[] args) {
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
 
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) throws NacosException {
+        String serverAddr = "localhost:8848";
+        String dataId = "test.yml";
+        String group = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put("serverAddr", serverAddr);
+        ConfigService configService = NacosFactory.createConfigService(properties);
+        String config = configService.getConfig(dataId, group, 5000);
+        System.out.println("config = " + config);
     }
 }
